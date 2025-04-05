d9f2c988dde38ee8fdbd5af383ca9dc1d49e0bcf
saikiran-badde <saikirankumarbadde97@gmail.com>
2025-04-05 23:28:18 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEP1-Kxz10v6MuMzVMi-AaLs3gNZNfdaI",
+  authDomain: "kevin-267bc.firebaseapp.com",
+  projectId: "kevin-267bc",
+  storageBucket: "kevin-267bc.firebasestorage.app",
+  messagingSenderId: "302532711334",
+  appId: "1:302532711334:web:7edf7e82176e4466f0f567",
+  measurementId: "G-XSCMPQ3DWK"
+};
+
+firebase.initializeApp(firebaseConfig);
